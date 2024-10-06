173570b9de1ff8049c96c876cda76bf486877e5b
Lucas <lucasoliveirars22@gmail.com>
2024-10-06 10:32:32 -0300

    fix: migration

--- api/src/main/resources/db/migration/V3__create_address_table.sql
+++ api/src/main/resources/db/migration/V3__create_address_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE address (
-    id UUID DEFAULT gen_random_int() PRIMARY KEY,
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     city VARCHAR(100) NOT NULL,
     uf VARCHAR(100) NOT NULL,
     event_id UUID,
